232ac5ab26707a95903527c2a250a1ec16c5cf4f
dpoarch <davidpoarch@outlook.com>
2017-09-28 16:55:13 +0800

    Upload Initial Files

---
+++ SpencerGifts.DAL/ConnectionManagerDataSection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SpencerGifts.ExchangeRatesUpdater.DAL
+{
+
+        public class ConnectionManagerDataSection : ConfigurationSection
+        {
+            /// <summary>
+            /// The name of this section in the app.config.
+            /// </summary>
+            public const string SectionName = "ConnectionManagerDataSection";
+
+            private const string EndpointCollectionName = "ConnectionManagerEndpoints";
+
+            [ConfigurationProperty(EndpointCollectionName)]
+            [ConfigurationCollection(typeof(ConnectionManagerEndpointsCollection), AddItemName = "add")]
+            public ConnectionManagerEndpointsCollection ConnectionManagerEndpoints { get { return (ConnectionManagerEndpointsCollection)base[EndpointCollectionName]; } }
+        }
+
+        public class ConnectionManagerEndpointsCollection : ConfigurationElementCollection
+        {
+            protected override ConfigurationElement CreateNewElement()
+            {
+                return new ConnectionManagerEndpointElement();
+            }
+
+            protected override object GetElementKey(ConfigurationElement element)
+            {
+                return ((ConnectionManagerEndpointElement)element).Name;
+            }
+        }
+
+        public class ConnectionManagerEndpointElement : ConfigurationElement
+        {
+            [ConfigurationProperty("name", IsRequired = true)]
+            public string Name
+            {
+                get { return (string)this["name"]; }
+                set { this["name"] = value; }
+            }
+
+            [ConfigurationProperty("rateParam", IsRequired = true)]
+            public string RateParam
+            {
+                get { return (string)this["rateParam"]; }
+                set { this["rateParam"] = value; }
+            }
+
+            [ConfigurationProperty("dateParam", IsRequired = false)]
+            public string DateParam
+            {
+                get { return (string)this["dateParam"]; }
+                set { this["dateParam"] = value; }
+            }
+
+            [ConfigurationProperty("dateFormat", IsRequired = false)]
+            public string DateFormat
+            {
+                get { return (string)this["dateFormat"]; }
+                set { this["dateFormat"] = value; }
+            }
+
+            [ConfigurationProperty("query", IsRequired = false)]
+            public string Query
+            {
+                get { return (string)this["query"]; }
+                set { this["query"] = value; }
+            }
+
+            [ConfigurationProperty("executeQuery", IsRequired = false, DefaultValue = false)]
+            public bool ExecuteQuery
+            {
+                get { return (bool)this["executeQuery"]; }
+                set { this["executeQuery"] = value; }
+            }
+        }
+  
+}
---
+++ SpencerGifts.ExchangeRatesUpdater/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using SpencerGifts.ExchangeRatesUpdater.DAL;
+
+namespace SpencerGifts.ExchangeRatesUpdater
+{
+   class Program
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+     
+
+        static int Main(string[] args)
+        {
+           
+            string exchangeRate = string.Empty;
+            string dateUpdated = string.Empty;
+
+            //Prepare parameters
+            try
+            {
+
+                foreach (string arg in args)
+                {
+
+                    int commandIndex = arg.IndexOf(":");
+                    string command = arg.Substring(0, commandIndex).ToUpper();
+
+                    switch (command)
+                    {
+                        case "EXCHANGERATE":
+                            exchangeRate = arg.Substring(commandIndex + 1, arg.Length - commandIndex - 1);
+                            break;
+                        case "DATEUPDATED":
+                            dateUpdated = arg.Substring(commandIndex + 1, arg.Length - commandIndex - 1);
+                            break;
+                        default:
+                            // do other stuff...
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Debug(ex);
+                _logger.ErrorException("[" + DateTime.Now.ToString() + "]", ex);
+            }
+
+             
+               
+            
+            //Start Updating
+            try
+            {
+
+                _logger.Info("Exchange Rate Started");
+
+                var connectionManagerDataSection = ConfigurationManager.GetSection(ConnectionManagerDataSection.SectionName) as ConnectionManagerDataSection;
+                if (connectionManagerDataSection != null)
+                {
+                    foreach (ConnectionManagerEndpointElement endpointElement in connectionManagerDataSection.ConnectionManagerEndpoints)
+                    {
+                       if (endpointElement.ExecuteQuery)
+                        {
+                            DBHelper _dbHelper = new DBHelper(endpointElement.Name);    
+                            DateTime oDate = Convert.ToDateTime(dateUpdated);
+                            DBParameter param1 = new DBParameter(endpointElement.RateParam, exchangeRate);
+                            DBParameter param2 = new DBParameter(endpointElement.DateParam, oDate.ToString(endpointElement.DateFormat));
+
+                            DBParameterCollection paramCollection = new DBParameterCollection();
+                            IDbTransaction transaction = _dbHelper.BeginTransaction();
+                            paramCollection.Add(param1);
+                            paramCollection.Add(param2);
+                            string message = "";
+                            string updateCommand = endpointElement.Query;
+                            try
+                            {
+                                message = _dbHelper.ExecuteNonQuery(updateCommand, paramCollection, transaction) > 0 ? "Exchange Rate updated successfully." : "Error in updating record.";
+                                _dbHelper.CommitTransaction(transaction);
+                                _logger.Info(endpointElement.Name + "Database Updated Successfully.");
+                            }
+                            catch (Exception err)
+                            {
+                                _dbHelper.RollbackTransaction(transaction);
+                            }
+
+                        }
+
+                    }
+                }
+
+                _logger.Info("Exchange Rate Ended");
+
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                _logger.Info("Exchange Rate Stoped");
+                _logger.ErrorException("Exception: ", ex);
+                return 0;
+            }
+
+
+        }
+    }
+}
